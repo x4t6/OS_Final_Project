261660e344dd306d98e2d7ad21e8488064d6b178
XXXXTTTTTT <liao655110@gmail.com>
2018-01-01 20:55:02 +0800

    thread test
    基本的thread 理解 測試

--- OS_SP_Problem/main.cpp
+++ OS_SP_Problem/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
-int main()
+void test_func()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  // do something
+  double dSum = 0;
+  for( int i = 0; i < 10000; ++ i )
+    for( int j = 0; j < 10000; ++ j )
+      dSum += i*j;
+
+  cout << "Thread: " << dSum << endl;
+}
+
+int main( int argc, char** argv )
+{
+  // execute thread
+  thread mThread( test_func );
+
+  // do somthing
+  cout << "main thread" << endl;
+
+  // wait the thread stop
+  mThread.join();
+
+  return 0;
 }
