32db651dc8be53bc14f1d0dd945a1c15a663e898
XXXXTTTTTT <liao655110@gmail.com>
2018-01-01 23:12:26 +0800

    ???
    ???

--- test_func/mytest/main.cpp
+++ test_func/mytest/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
-int main()
+void OutputValue( int n )
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  cout << "Number:";
+  for( int i = 0; i < n; ++ i )
+  {
+    this_thread::sleep_for( chrono::duration<int, std::milli>( 5 ) );
+    cout << " " << i;
+  }
+  cout << endl;
+}
+
+int main( int argc, char** argv )
+{
+  cout << "Normal function call" << endl;
+  OutputValue( 3 );
+  OutputValue( 4 );
+
+  cout << "\nCall function with thread" << endl;
+  thread mThread1( OutputValue, 3 );
+  thread mThread2( OutputValue, 4 );
+  mThread1.join();
+  mThread2.join();
+  cout << endl;
+
+  return 0;
 }
