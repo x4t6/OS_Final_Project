e34dbb2b88a8a13435b2bc4e3eeb20c9fc100542
XXXXTTTTTT <liao655110@gmail.com>
2018-01-10 03:01:25 +0800

    starting
    staring

--- OS_SP_Problem/main.cpp
+++ OS_SP_Problem/main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
 
 using namespace std;
 
-void test_func()
-{
-  // do something
-  double dSum = 0;
-  for( int i = 0; i < 10000; ++ i )
-    for( int j = 0; j < 10000; ++ j )
-      dSum += i*j;
+mutex mtx;                      // critical section的互斥鎖
+condition_variable cv;          // 互斥鎖的condition variable
+bool ready = false;             // 告訴 threadperson t 準備好了
+int check_next = 0;             // check_next, count , person把權力交給下一個person
 
-  cout << "Thread: " << dSum << endl;
-}
+class USER
+{
+private:
+
+public:
+    int atfloor;
+    int tofloor;
+    int dir=0;              //方向
+    int order_flag=0;       //發過指令的人，flag會變成1
+    int inelevator_flag=0;   //人在在電梯裡為1
+    int id=0;
+    char name;
+    int live=0;             //為1為該user會出現
+    int color;
+};
+
 
 int main( int argc, char** argv )
 {
